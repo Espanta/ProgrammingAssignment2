6df1ea3ddc9604f52f8d4f154eb6dcb78e16c6e2
Espanta <abolfazli.s@gmail.com>
2015-06-20 21:19:58 -0700

    Completed the assignment
    Completed the given assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    rev <- NULL
+    
+    getMatrix <- function() x
+    setReverse <- function(reverse) rev <<- reverse
+    getReverse <- function() rev
+    
+    list( getMatrix = getMatrix,
+          setReverse = setReverse,
+          getReverse = getReverse)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    rev <- x$getReverse()
+    if(!is.null(rev)) {
+        message("getting cached data")
+        return(rev)
+    }
+    data <- x$getMatrix()
+    rev <- solve(data)
+    x$setReverse(rev)
+    rev
 }
